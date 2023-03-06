ee4088113bc272854b42c9d1810c3a4c29865fe6
Cátia Martins da silva <catia12m@gmail.com>
2023-03-06 09:04:32 -0300

    exercicio 011

--- Aula 11/ex011.js
+++ Aula 11/ex011.js
@@ -1,1 +1,9 @@
-console.log('o console executou corretamente')
+console.log(`o console executou corretamente`)
+var vel=75
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if(vel>60){//condição simples
+    console.log(`você ultrapassou a velocidade permitida.MULTADO`)  
+}
+
+console.log(`Dirija com cinto de segurança`)
+
